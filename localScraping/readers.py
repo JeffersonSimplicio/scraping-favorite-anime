b520cb9bd0e152be880349a63e6daaf7affe10a7
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-27 20:36:36 -0300

    feat: create readWeb

--- localScraping/readers.py
+++ localScraping/readers.py
@@ -1,5 +1,14 @@
+import requests
+
+
 def readFile(path: str) -> str:
     data = open(path, 'r')
     code = data.read()
     data.close()
     return code
+
+
+def readWeb(url: str) -> str:
+    page = requests.get(url)
+    html_content = page.text
+    return html_content
