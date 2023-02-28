b92d24b445bc95d94fa4897e2dfe4d8119782569
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2023-02-27 21:14:39 -0300

    feat: create list_favorites_name

--- localScraping/main.py
+++ localScraping/main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from readers import readFile
 
 
 class FavoritesAnimes:
@@ -10,3 +11,24 @@ class FavoritesAnimes:
 
     def len_favorites(self) -> int:
         return len(self.__code.find_all('article'))
+
+    def list_favorites_name(self) -> list:
+        favorites_list = self.__code.find_all(
+            'div',
+            {"class": "card-vertical-title"}
+        )
+        names_list = [divName.string for divName in favorites_list]
+        return names_list
+
+
+# Test Zone
+if __name__ == '__main__':
+    code = readFile('favorites-list-page.html')
+    soup = BeautifulSoup(code, "html.parser")
+
+    betterAnime = FavoritesAnimes(soup)
+
+    list = betterAnime.list_favorites_name()
+
+    for name in list:
+        print(name)
