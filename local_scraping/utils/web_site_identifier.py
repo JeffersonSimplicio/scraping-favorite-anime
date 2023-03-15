293bbcd40e90cbefde24513acfef34f194c3562a
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-15 11:18:58 -0300

    refactor: organize file

--- local_scraping/utils/web_site_identifier.py
+++ local_scraping/utils/web_site_identifier.py
@@ -9,13 +9,3 @@ def web_site_identifier(soup: BeautifulSoup):
     raise ValueError(
         'The file does not belong to any of the supported formats'
     )
-
-
-if __name__ == '__main__':
-    from utils.readers import readFile
-
-    code = readFile('pages/anihub/anihub_page.html')
-    soup = BeautifulSoup(code, "html.parser")
-
-    x = web_site_identifier(soup)
-    print(type(x))
