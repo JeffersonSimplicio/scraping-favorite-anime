21d2505080be482fbe35cf1055a955ec9193c761
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-16 23:12:25 -0300

    feat: Portuguese correction

--- src/interface/interface_CLI.py
+++ src/interface/interface_CLI.py
@@ -13,10 +13,16 @@ def interface():
     soup = BeautifulSoup(code, "html.parser")
 
     filter = select_filter(soup)
-
     filterAnime = FavoritesAnimes(filter, soup)
 
-    print(f'Você possui {filterAnime.len_favorites()} animes favoritos')
+    total_favorites = filterAnime.len_favorites()
+
+    if (total_favorites == 0):
+        print('Você não possui nenhum anime na lista de favoritos')
+    elif (total_favorites == 1):
+        print('Você possui 1 anime na lista de favoritos')
+    else:
+        print(f'Você possui {filterAnime.len_favorites()} animes favoritos')
 
     selected = select_option()
     operation(filterAnime, selected)
