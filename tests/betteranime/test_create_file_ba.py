7b1a7a212f8751de6c547fca1e4313c0da7335d5
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2023-03-20 09:06:03 -0300

    test: name and thunb list creation

--- tests/betteranime/test_create_file_ba.py
+++ tests/betteranime/test_create_file_ba.py
@@ -26,3 +26,26 @@ Anime5
     remove("name_favorites_betteranime.txt")
 
     assert content == expectation
+
+
+def test_name_thunb_list_creation():
+    expectation = """Anime1 - https://betteranime.com/anime1-img.jpg
+
+Anime2 - https://betteranime.com/anime2-img.jpg
+
+Anime3 - https://betteranime.com/anime3-img.jpg
+
+Anime4 - https://betteranime.com/anime4-img.jpg
+
+Anime5 - https://betteranime.com/anime5-img.jpg
+
+"""
+
+    better_anime.create_txt_list_favorites_url()
+
+    file = open("name_url_favorites_betteranime.txt", mode="r")
+    content = file.read()
+    file.close()
+    remove("name_url_favorites_betteranime.txt")
+
+    assert content == expectation
